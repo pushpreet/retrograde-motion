b7bc14a3411688056b97c249b376ecf334f81572
pushpreet <hanspal.pushpreet@gmail.com>
2013-10-06 01:19:11 +0530

    Temporary main file
    To test the working of the class orbiter

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <windows.h>
+#include <dos.h>
+#include "orbiter.h"
+
+using namespace std;
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int main( )
+{
+	orbiter orb;
+	orb.setOrbitRadius( 10.0 );
+	orb.setVelocity(10.0,'a');
+
+	for (double i = 0.0; i < 100; i+=0.01)
+	{
+		orb.calcPosition( i ) ;
+		gotoxy(10+orb.getPos().x,10+orb.getPos().y);
+		cout<<".";
+		Sleep(500);
+	}
+
+	return 0;
+}
